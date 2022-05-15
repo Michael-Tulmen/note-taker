37fbd6737a3d452123f053324da66dfe8a62c295
Michael Tulmen <michaeltulmen@gmail.com>
2022-05-15 16:49:14 -0500

    moved port

--- server.js
+++ server.js
@@ -8,9 +8,8 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-
-const app = express();
 const port = process.env.PORT || 3001;
+const app = express();
 const mainDir = path.join(__dirname, "/public");
 
 app.use(express.static('public'));
