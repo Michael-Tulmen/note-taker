60288e37b677472255bfbce771eb4d2c0234007b
Michael Tulmen <michaeltulmen@gmail.com>
2022-05-16 23:28:28 -0500

    finalized the program, comments added all files, explained everything I did

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
-const res = require('express/lib/response');
-const {notes} = require('../../db/db');
+const {notes} = require('../../db/db.json');
 const {createNote,deleteNote} = require('../../lib/noteFunc');
 
 //problem with this relating to DB
@@ -8,16 +7,13 @@ router.get('/notes', (req,res) => {
     let saved = notes;
     res.json(saved);
 })
-
 router.post('/notes', (req,res) => {
     req.body.id = notes.length.toString();
     let note = createNote(req.body, notes);
     res.json(note);
 })
-
 router.delete('/notes/:id', (req,res) => {
     deleteNote(notes, req.params.id);
     res.json(notes);
 })
-
 module.exports = router;
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,10 +1,11 @@
 const path = require('path');
 const router = require('express').Router();
-
+//this is the main endpoint where the notes will come from
 router.get('/notes', (req,res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
+//catchall
 router.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 module.exports = router;
